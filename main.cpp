935d56182554c69186b3a968aed467064100de09
lone wolf <lone_wolf_akela@qq.com>
2021-07-11 14:45:50 +0800

    finished

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <format>
 #include <string>
+#include <string_view>
 
 #include <icu.h>
 #pragma comment(lib, "icu.lib") 
@@ -10,31 +11,27 @@
 #undef TRUE
 
 
-constexpr char16_t BOM = u'\ufeff';
-
-constexpr char GB2312_LIST_FILE[] = "gb2312list.txt";
-constexpr char OUTPUT_LUA_FILE[] = "utf16_to_gb2312.lua";
-
-std::string char16_to_gb2312(char16_t c)
+std::string char16_to_ansi(char16_t c, const std::string& codepage)
 {
 	std::string result(2, '0');
 	UErrorCode err = U_ZERO_ERROR;
-	UConverter* conv = ucnv_open("gb2312", &err);
+	UConverter* conv = ucnv_open(codepage.c_str(), &err);
 	int32_t len = ucnv_fromUChars(conv, result.data(), result.size(), &c, 1, &err);
 	if (err > U_ZERO_ERROR)
 	{
-		std::cerr << "char16_to_utf8 failed.\n";
+		std::cerr << "char16_to_ansi failed.\n";
 		std::abort();
 	}
 	result.resize(len);
 	return result;
 }
 
-int main()
+std::string gen_char_table(const std::string& char_list_file, const std::string& codepage)
 {
-	std::cout << std::format("{}\n", "Hello, world!");
+	constexpr char16_t BOM = u'\ufeff';
 
-	std::ifstream char_list(GB2312_LIST_FILE, std::ios::binary);
+	std::string char_table = "";
+	std::ifstream char_list(char_list_file, std::ios::binary);
 	if (!char_list)
 	{
 		std::cerr << "failed to open input file.\n";
@@ -47,20 +44,69 @@ int main()
 		std::cerr << "first char is not BOM.\n";
 		std::abort();
 	}
+	while (char_list.peek() != EOF)
+	{
+		char_list.read(reinterpret_cast<char*>(&c), sizeof(c));
+		const auto gbstr = char16_to_ansi(c, codepage);
+		if (gbstr.size() == 2)
+		{
+			char_table += std::format("\t\t[{}] = \"\\{:0>3}\\{:0>3}\",\n", uint16_t(c), uint8_t(gbstr[0]), uint8_t(gbstr[1]));
+		}
+		else if (gbstr.size() == 1)
+		{
+			char_table += std::format("\t\t[{}] = \"\\{:0>3}\",\n", uint16_t(c), uint8_t(gbstr[0]));
+		}
+		else if (gbstr.size() == 0)
+		{
+			continue;
+		}
+		else
+		{
+			std::cerr << "Wrong code length.\n";
+			std::abort();
+		}
+	}
+	return char_table;
+}
 
-	std::ofstream lua_script(OUTPUT_LUA_FILE, std::ios::binary);
-	if (!lua_script)
+void gen_script(const std::string& char_list_file, const std::string& script_file, const std::string& codepage)
+{
+	std::string script = std::format(R"(function conv_chatname(chatname)
+	local char_table = {{
+{}
+	}}
+	if type(chatname) == "string" then
+		return chatname
+	elseif type(chatname) == "table" then
+		local result = ""
+		for i, c in chatname do
+			if c == 0 then
+				break
+			end
+			result = result..(char_table[c] or "?")
+		end
+		return result
+	else
+		_ALERT("Wrong chatname format!")
+		return "?"
+	end
+end
+)",
+gen_char_table(char_list_file, codepage));
+
+	std::ofstream lua_script_gb2312(script_file, std::ios::binary);
+	if (!lua_script_gb2312)
 	{
 		std::cerr << "failed to open output file.\n";
 		std::abort();
 	}
-	lua_script << "char16_to_gb2312_table = {\n";
-	while (char_list.peek() != EOF)
-	{
-		char_list.read(reinterpret_cast<char*>(&c), sizeof(c));
-		const auto gbstr = char16_to_gb2312(c);
-		lua_script << std::format("\t[{}] = \"\\{:0>3}\\{:0>3}\",\n", uint16_t(c), uint8_t(gbstr[0]), uint8_t(gbstr[1]));
-	}
-	lua_script << "}\n";
+	lua_script_gb2312 << script;
+}
+
+int main()
+{
+	std::cout << std::format("{}\n", "Hello, world!");
+	gen_script("gb2312list.txt", "utf16_to_gb2312.lua", "gb2312");
+	gen_script("english.txt", "utf16_to_latin1.lua", "Latin 1");
 	return 0;
 }
--- test.lua
+++ test.lua
@@ -1,2 +1,41 @@
 dofile("utf16_to_gb2312.lua")
-_ALERT(char16_to_gb2312_table[30005])
+
+execute("chcp 936")
+
+chatname = {
+  30005,
+  33041,
+  49,
+  0,
+}
+print(conv_chatname(chatname)) -- 电脑1
+
+chatname = "lone_wolf_rpgtes"
+print(conv_chatname(chatname)) -- lone_wolf_rpgtes
+
+chatname = {
+  30005,
+  0,
+  49,
+  0,
+}
+print(conv_chatname(chatname)) -- 电
+
+chatname = {
+  12,
+  33041,
+  49,
+  0,
+}
+print(conv_chatname(chatname)) -- ?脑1
+
+execute("pause")
+execute("chcp 1252")
+dofile("utf16_to_latin1.lua")
+chatname = {
+  214,
+  214,
+  214,
+  0,
+}
+print(conv_chatname(chatname)) -- ÖÖÖ
